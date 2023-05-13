07f6596aef2f0b0c78097c27ba111cf8979cdb18
angov-vangel <angov.vangel@gmail.com>
2023-05-14 01:05:00 +0200

    chart added

---
+++ components/Charts.tsx
@@ -0,0 +1,78 @@
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from "chart.js";
+  import { Bar } from "react-chartjs-2";
+  import { faker } from "@faker-js/faker";
+  
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  const DashboardPage = () => {
+    const options = {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top" as const,
+        },
+        title: {
+          display: true,
+          text: "Chart.js Bar Chart",
+        },
+      },
+    };
+  
+    const labels = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+    ];
+  
+    const data = {
+      labels,
+      datasets: [
+        {
+          label: "Dataset 1",
+          data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
+          backgroundColor: "rgba(255, 99, 132, 0.5)",
+        },
+        {
+          label: "Dataset 2",
+          data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
+          backgroundColor: "rgba(53, 162, 235, 0.5)",
+        },
+      ],
+    };
+  
+    return (
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+        <div className="grid grid-cols-2 gap-8">
+          <div>
+            <h2 className="text-lg font-medium mb-4">Number of Applicants</h2>
+            <Bar data={data} options={options} />
+          </div>
+          <div>
+            <h2 className="text-lg font-medium mb-4">Other Statistics</h2>
+            {/* Add other statistics here */}
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default DashboardPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import Charts from "@/components/Charts";
 import CreateEvent from "@/components/CreateEvent";
 import { Footer } from "@/components/Footer";
 import { LoginForm } from "@/components/LoginForm";
 import { LogoPlaceholder } from "@/components/LogoPlaceholder";
 import UserFormRegister from "@/components/UserFormRegister";
 import React from "react";
+import { Chart } from "react-chartjs-2";
 export default function Home() {
   return (
     <div className="h-[100vh]">
@@ -18,7 +20,7 @@ export default function Home() {
           <CreateEvent />
         </div>
       </div>
-
+      <Charts />
       <Footer />
     </div>
   );
