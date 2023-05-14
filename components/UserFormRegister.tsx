4c100268f85210b3373c8b1039245c690fa51464
angov-vangel <angov.vangel@gmail.com>
2023-05-14 09:50:34 +0200

    types changed to any for testing purposes

--- components/UserFormRegister.tsx
+++ components/UserFormRegister.tsx
@@ -57,7 +57,7 @@ const RegistrationForm = () => {
   useEffect(() => {
     const fetchAcademies = async () => {
       try {
-        const response = await axios.get<Academy[]>(
+        const response = await axios.get<any>(
           "https://mimica-kuzmanovska.sharedwithexpose.com/api/academies"
         );
         setAcademies(response.data);
@@ -74,7 +74,7 @@ const RegistrationForm = () => {
     if (selectedAcademy) {
       const fetchFoodPreferences = async () => {
         try {
-          const response = await axios.get<string[]>(
+          const response = await axios.get<any>(
             `https://mimica-kuzmanovska.sharedwithexpose.com/api/food-preferences`
           );
           setFoodPreferences(response.data);
